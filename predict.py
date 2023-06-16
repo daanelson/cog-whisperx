bc9ef6ce09033be0a2a81f74dfec3c65c7421769
Dan <dan.nelson8@gmail.com>
2023-06-16 23:48:42 +0000

    jax

--- predict.py
+++ predict.py
@@ -1,36 +1,30 @@
 # Prediction interface for Cog ⚙️
 # https://github.com/replicate/cog/blob/main/docs/python.md
 import os
-os.environ['HF_HOME'] = '/src/hf_models'
-os.environ['TORCH_HOME'] = '/src/torch_models'
-from cog import BasePredictor, Input, Path
-import torch
-import whisperx
+os.environ["HF_HOME"] = "/src/hf_models"
+os.environ["TORCH_HOME"] = "/src/torch_models"
 from typing import Any
 
+from cog import BasePredictor, Input, Path
+from whisper_jax import FlaxWhisperPipline
 
-compute_type="float16"
+
 class Predictor(BasePredictor):
     def setup(self):
         """Load the model into memory to make running multiple predictions efficient"""
-        self.device = "cuda"
-        self.model = whisperx.load_model("/src/converted_model", self.device, language="hi", compute_type=compute_type)
+        self.pipe = FlaxWhisperPipline(
+            "vasista22/whisper-hindi-large-v2", batch_size=16
+        )
+        self.pipe.model.config.forced_decoder_ids = (
+            self.pipe.tokenizer.get_decoder_prompt_ids(language="hi", task="transcribe")
+        )
 
     def predict(
         self,
         audio: Path = Input(description="Audio file"),
-        batch_size: int = Input(description="Parallelization of input audio transcription", default=32),
-        only_text: bool = Input(description="Set if you only want to return text; otherwise, segment metadata will be returned as well.", default=False),
-        debug: bool = Input(description="Print out memory usage information.", default=False)
     ) -> Any:
-        """Run a single prediction on the model"""
-        with torch.inference_mode():
-            result = self.model.transcribe(str(audio), batch_size=batch_size) 
-            # result is dict w/keys ['segments', 'language']
-            # segments is a list of dicts,each dict has {'text': <text>, 'start': <start_time_msec>, 'end': <end_time_msec> }
-            if only_text:
-                return ''.join([val['text'] for val in result['segments']])
-            if debug:
-                print(f"max gpu memory allocated over runtime: {torch.cuda.max_memory_reserved() / (1024 ** 3):.2f} GB")
-        return result['segments']
+        print("transcribing")
+        audio = str(audio)
+        transcription = self.pipe(audio)
 
+        return transcription["text"]
